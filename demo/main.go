a684840df8a63e9e342f5aab382b6e67de4a7020
linyiwei <1542046858@qq.com>
2024-10-19 14:27:55 +0800

    feat:测试

--- demo/main.go
+++ demo/main.go
@@ -6,6 +6,54 @@ import (
 
 func main() {
 	slice()
+	chanOperator()
+}
+
+func chanOperator() {
+	number := make(chan bool)
+	letter := make(chan bool)
+	done := make(chan bool)
+
+	go func() {
+		i := 1
+		for {
+			select {
+			case <-number:
+				fmt.Print(i)
+				i++
+				fmt.Print(i)
+				i++
+				letter <- true
+			}
+		}
+	}()
+
+	go func() {
+		j := 'A'
+		for {
+			select {
+			case <-letter:
+				if j >= 'Z' {
+					done <- true
+				} else {
+					fmt.Print(string(j))
+					j++
+					fmt.Print(string(j))
+					j++
+					number <- true
+				}
+			}
+		}
+	}()
+
+	number <- true
+
+	for {
+		select {
+		case <-done:
+			return
+		}
+	}
 }
 
 func slice() {
